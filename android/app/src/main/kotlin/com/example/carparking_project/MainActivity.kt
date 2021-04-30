8dc8966a8e2fb27bbf0200c05a8012f949794f1c
tanat29 <tanat29.mongkon@gmail.com>
2021-04-30 21:58:37 +0700

    Flutter Count Up Time

---
+++ android/app/src/main/kotlin/com/example/carparking_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.carparking_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _isHours = true;
+
+  final StopWatchTimer _stopWatchTimer = StopWatchTimer(
+    mode: StopWatchMode.countUp,
+    onChange: (value) => print('onChange $value'),
+    onChangeRawSecond: (value) => print('onChangeRawSecond $value'),
+    onChangeRawMinute: (value) => print('onChangeRawMinute $value'),
+  );
+
+  final _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _stopWatchTimer.rawTime.listen((value) =>
+        print('rawTime $value ${StopWatchTimer.getDisplayTime(value)}'));
+    _stopWatchTimer.minuteTime.listen((value) => print('minuteTime $value'));
+    _stopWatchTimer.secondTime.listen((value) => print('secondTime $value'));
+    _stopWatchTimer.records.listen((value) => print('records $value'));
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await _stopWatchTimer.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CountUp Time'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(40),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: StreamBuilder<int>(
+                      stream: _stopWatchTimer.rawTime,
+                      initialData: _stopWatchTimer.rawTime.valueWrapper?.value,
+                      builder: (context, snap) {
+                        final value = snap.data!;
+                        final displayTime = StopWatchTimer.getDisplayTime(value,
+                            hours: _isHours);
+                        return Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(0),
+                              child: Text(
+                                displayTime,
+                                style: const TextStyle(
+                                    fontSize: 72,
+                                    fontFamily: 'Helvetica',
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 4),
+                          child: RaisedButton(
+                            padding: const EdgeInsets.all(2),
+                            color: Colors.yellow[600],
+                            shape: const StadiumBorder(),
+                            onPressed: () async {
+                              _stopWatchTimer.onExecute
+                                  .add(StopWatchExecute.start);
+                            },
+                            child: const Text(
+                              'Start',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 4),
+                          child: RaisedButton(
+                            padding: const EdgeInsets.all(2),
+                            color: Colors.yellow[600],
+                            shape: const StadiumBorder(),
+                            onPressed: () async {
+                              _stopWatchTimer.onExecute
+                                  .add(StopWatchExecute.stop);
+                            },
+                            child: const Text(
+                              'Stop',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 4),
+                          child: RaisedButton(
+                            padding: const EdgeInsets.all(2),
+                            color: Colors.yellow[600],
+                            shape: const StadiumBorder(),
+                            onPressed: () async {
+                              _stopWatchTimer.onExecute
+                                  .add(StopWatchExecute.reset);
+                            },
+                            child: const Text(
+                              'Reset',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
